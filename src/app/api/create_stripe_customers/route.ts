5bf59ab883c9dbec8f2b62f04c04e6cf8733b793
bilecky <bilecky95@gmail.com>
2024-12-02 14:16:51 +0100

    added security check to backend API Route

--- src/app/api/create_stripe_customers/route.ts
+++ src/app/api/create_stripe_customers/route.ts
@@ -1,13 +1,31 @@
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+import { createClient } from "@/utils/supabase/server";
 
 // Inicjalizacja Stripe poza handlerem, aby zachować singleton
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
+const API_KEY = process.env.API_ROUTE_SECRET;
+
+if (!API_KEY) {
+  throw new Error("API KEY NIE ZOSTAL ZNALEZIONY!");
+}
+
 export async function POST(req: NextRequest) {
+  const apiKey = req.nextUrl.searchParams.get("API_ROUTE_SECRET");
+
+  // Porównanie klucza z tym zapisanym w env
+  if (!apiKey || apiKey !== API_KEY) {
+    return NextResponse.json(
+      { error: "Nie jestes autoryzowany aby korzystac z API" },
+      { status: 401 },
+    );
+  }
+  const supabase = await createClient();
+
   try {
     // Parsowanie danych z żądania
-    const { email } = await req.json();
+    const { email, id } = await req.json();
 
     // Walidacja danych wejściowych
     if (!email) {
@@ -19,6 +37,14 @@ export async function POST(req: NextRequest) {
 
     // Tworzenie nowego klienta Stripe
     const customer = await stripe.customers.create({ email: email });
+
+    // Zapisywanie identyfikatora klienta w bazie danych
+    await supabase
+      .from("profile")
+      .update({
+        stripe_customer: customer.id,
+      })
+      .eq("id", id);
 
     // Zwracanie identyfikatora nowego klienta
     return NextResponse.json(
