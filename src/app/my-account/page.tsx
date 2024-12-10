db90ddcec1d5f5d1f03f601ee4418d3115c3a9f7
bilecky <bilecky95@gmail.com>
2024-12-10 16:27:44 +0100

    added logout functionality

--- src/app/my-account/page.tsx
+++ src/app/my-account/page.tsx
@@ -4,6 +4,8 @@ import { useUser } from "../context";
 import { createClient } from "@/utils/supabase/client";
 import { redirectToStripeCheckout } from "@/lib/utils";
 import { getProduct } from "@/lib/actions";
+import { useRouter } from "next/navigation";
+import { IoMdLogOut } from "react-icons/io";
 
 type Props = {};
 
@@ -19,6 +21,7 @@ const page = (props: Props) => {
   );
   const [product, setProduct] = useState<Record<string, any> | null>(null);
   const productPriceID = product?.price.id;
+  const router = useRouter();
 
   const supabase = createClient();
   const { user } = useUser();
@@ -43,14 +46,23 @@ const page = (props: Props) => {
     fetchData();
   }, []);
 
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Błąd podczas wylogowywania:", error);
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
-    <section className="background-mesh-generated-hero h-[110vh]">
+    <section className="background-mesh-generated-hero h-auto md:h-[110vh]">
       <div className="section_wrapper container relative z-10 flex h-full w-full rounded-xl pb-32 pt-56 text-center">
         <div className="inside_wrapper header-glassmorphism h-full w-full rounded-xl">
           <h1 className="py-8 text-4xl font-semibold">Moje konto</h1>
 
-          <div className="sections_wrapper grid h-full w-full md:grid-cols-2">
-            <div className="section_left p-8 text-left">
+          <div className="sections_wrapper md:grid-rows-auto grid h-full w-full md:h-5/6 md:grid-cols-2">
+            <div className="section_left flex h-full flex-col p-8 text-left">
               <div className="header-info flex items-center justify-center">
                 <h2 className="mr-4 whitespace-nowrap text-xl font-semibold">
                   Informacje ogólne
@@ -93,7 +105,19 @@ const page = (props: Props) => {
                 )}
               </ul>
             </div>
-            <div className="section_right"> </div>
+            <div className="section_right h-full p-8">
+              <div className="logout_wrapper flex h-full w-full items-end justify-end">
+                <button
+                  onClick={handleLogout}
+                  className="text-md flex items-center justify-center rounded-xl border border-primary bg-primary px-6 py-3 font-semibold text-primary-DEFAULT_PURPLE_FONT_COLOR transition duration-300 hover:bg-primary-DARKENED_PURPLE_BG hover:text-primary md:text-xl"
+                >
+                  <span className="mr-3">
+                    <IoMdLogOut className="text-xl md:text-3xl" />
+                  </span>
+                  Wyloguj się
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
