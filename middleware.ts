9bbb04c6f21a4693795ce9a3775674f766cac216
bilecky <bilecky95@gmail.com>
2024-12-16 18:31:21 +0100

    moved middlewares (works local, not on prod) to root folder -- check if work on production now

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,8 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
+  console.log("Middleware triggered for:", request.nextUrl.pathname);
+
   return await updateSession(request);
 }
 
